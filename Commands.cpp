2af2db054f964d6cf72ca01637ede7493d2a6db5
JZ92 <33635434+JZ92@users.noreply.github.com>
2017-11-15 22:12:17 +0200

    Add files via upload

--- Commands.cpp
+++ Commands.cpp
@@ -1,5 +1,46 @@
 #include "../include/Commands.h"
 
+Directory * goTo (Directory * location, string args)
+{
+    string sub;
+    bool loop=false;
+    size_t found;
+    while (!loop) {
+        found=args.find("/");
+        if (found==-1)
+            sub=args;
+        else
+            sub = args.substr(0, found);
+
+        if (sub == "..") {
+            if ((location&).getParent() == nullptr) {
+                return nullptr;
+            }
+            else
+                (location&)=(location&).getParent();
+        } else {
+            vector<BaseFile *>::iterator myIt;
+            bool found2 = false;
+            for (myIt = (location&).getChildren().begin();
+                 myIt != (location&).getChildren().end(); myIt++) {
+                if (sub.compare((**myIt).getName()) == 0 && !((**myIt).isFile()))
+                {
+                    location=**myIt;
+                    found2 = true;
+                }
+            }
+            if (!found2) {
+                return nullptr;
+            }
+        }
+        if (args == sub)
+            loop = true;
+        else
+            args = args.substr(found + 1);
+    }
+    return location;
+}
+
 BaseCommand:: BaseCommand(string args) : args(args) {}
 
 string BaseCommand:: getArgs()
@@ -10,7 +51,7 @@ string BaseCommand:: getArgs()
 PwdCommand:: PwdCommand(string args) : BaseCommand(args) {}
 void PwdCommand:: execute(FileSystem & fs)
 {
-    string path="/"";
+    string path="/";
     vector <string> output;
     Directory current=fs.getWorkingDirectory();
     output.push_back(current.getName());
@@ -35,52 +76,17 @@ CdCommand:: CdCommand(string args) :BaseCommand(args) {}
 void CdCommand:: execute(FileSystem & fs)
 {
     string args=getArgs();
-    string sub;
-    bool loop=false;
-    size_t found;
-    if (args.at(0)=='/') {
-        fs.setWorkingDirectory(&fs.getRootDirectory());
-        args = args.substr(1);
-    }
-
-    while (!loop) {
-        found=args.find("/");
-        if (found==-1)
-                sub=args;
+    Directory * current;
+    if (args.at(0)=='/')
+        current=goTo((&fs.getRootDirectory()),args.substr(1));
         else
-            sub = args.substr(0, found);
-
-            if (sub == "..") {
-                if (fs.getWorkingDirectory().getParent() == nullptr) {
-                    cout << "The system cannot find the path specified";
-                    loop = true;
-                }
-                else
-                    fs.setWorkingDirectory(fs.getWorkingDirectory().getParent());
-            } else {
-                vector<BaseFile *>::iterator myIt;
-                bool found2 = false;
-                for (myIt = fs.getWorkingDirectory().getChildren().begin();
-                     myIt != fs.getWorkingDirectory().getChildren().end(); myIt++) {
-                    if (sub.compare((**myIt).getName()) == 0 && !((**myIt).isFile()))
-                    {
-                        fs.setWorkingDirectory(**myIt);
-                        found2 = true;
-                    }
-                }
-                if (!found2) {
-                    cout << "The system cannot find the path specified";
-                    loop = true;
-                }
-            }
-            if (args == sub)
-                loop = true;
-            else
-                args = args.substr(found + 1);
-
-    }
-
+        current=goTo((&fs.getWorkingDirectory()),args);
+    if (current== nullptr)
+        cout << "The system cannot find the path specified";
+    else
+        fs.setWorkingDirectory(current);
 }
+
 string CdCommand:: toString() {return "cd";}
 
 
@@ -90,7 +96,7 @@ void LsCommand:: execute(FileSystem & fs)
 {
     string args=getArgs();
     bool sortBySize=false;
-    if (args.size()>=4 && args.substr(0,4).compare("[-s]"==0))
+    if (args.size()>=4 && args.substr(0,4).compare("[-s]")==0)
     {
         sortBySize=true;
         if (args!="[-s]")
@@ -99,65 +105,145 @@ void LsCommand:: execute(FileSystem & fs)
             args="";
     }
 
-    Directory current = fs.getWorkingDirectory();
-    if (args.at(0)=='/') {
-        current=fs.getRootDirectory();
-        args = args.substr(1);
-    }
-    size_t found;
-    string sub;
-    bool loop=false;
-    while (!loop) {
-        found=args.find("/");
-        if (found==-1)
-            sub=args;
+    Directory * current;
+    if (args.at(0)=='/')
+        current=goTo((&fs.getRootDirectory()),args.substr(1));
+    else
+        current=goTo((&fs.getWorkingDirectory()),args);
+    if (current== nullptr)
+        cout << "The system cannot find the path specified";
+    else {
+        if (sortBySize)    // SAME SIZE ISSUE (JONATHAN)
+            (*current).sortBySize();
         else
-            sub = args.substr(0, found);
-
-        if (sub == "..") {
-            if (current.getParent() == nullptr) {
-                cout << "The system cannot find the path specified";
-                loop = true;
-            }
+            (*current).sortByName();
+        vector<BaseFile *>::iterator printIt;
+        for (printIt = (*current).getChildren().begin(); printIt != (*current).getChildren().end(); printIt++) {
+            if ((**printIt).isFile())
+                cout << "FILE/t" << (**printIt).getName() << "/t" << (**printIt).getSize();
             else
-                current=(*current.getParent());
-        } else {
-            vector<BaseFile *>::iterator myIt;
-            bool found2 = false;
-            for (myIt = current.getChildren().begin();
-                 myIt != current.getChildren().end(); myIt++) {
-                if (sub.compare((**myIt).getName()) == 0 && !((**myIt).isFile()))
-                {
-                    current=**myIt;
-                    found2 = true;
-                }
-            }
-            if (!found2) {
-                cout << "The system cannot find the path specified";
-                loop = true;
-            }
+            cout << "DIR/t" << (**printIt).getName() << "/t" << (**printIt).getSize();
+
+            cout << "/n";
         }
-        if (args == sub)
-            loop = true;
-        else
-            args = args.substr(found + 1);
-
     }
-    if (sortBySize)    // SAME SIZE ISSUE (JONATHAN)
-        current.sortBySize();
-        else
-        current.sortByName();
-    vector <BaseFile*> :: iterator printIt;
-    for (printIt=current.getChildren().begin() ; printIt!=current.getChildren().end() ; printIt++)
-    {
-        if ((**printIt).isFile())
-            cout << "FILE/t" << (**printIt).getName()"/t"  << (**printIt).getSize();
-        else
-        cout << "DIR/t" << (**printIt).getName() << "/t"  << (**printIt).getSize();
-        cout << "/n";
-    }
-
 
 }
-string LsCommand:: toString() {return "ls";}
+
+    string LsCommand:: toString() {return "ls";}
+
+    MkdirCommand:: MkdirCommand(string args) : BaseCommand(args) {}
+    void MkdirCommand:: execute(FileSystem & fs)
+    {}
+    string MkdirCommand:: toString() {return "mkdir";}
+
+    MkfileCommand:: MkfileCommand(string args) :BaseCommand(args) {}
+    void MkfileCommand:: execute(FileSystem & fs)
+    {
+        string args=getArgs();
+        size_t found = args.find_last_of('/');
+        string sub;
+        size_t space;
+        string sizeW;
+        int size;
+        string path;
+        if (found!=-1)
+        {
+            sub=args.substr(found+1);
+            space=sub.find(' ');
+            sub=sub.substr(0,space);
+            sizeW=sub.substr(space+1);
+            size=atoi(sizeW);
+            path=args.substr(0,found);
+        }
+        else
+        {
+            space=args.find_last_of(' ');
+            sub=args.substr(0,space);
+            sizeW=sub.substr(space+1);
+            size=atoi(sizeW);
+            path=args.substr(0,space);
+        }
+        Directory * location;
+        if (args.at(0)=='/')
+            location = goTo((&fs.getRootDirectory()),path.substr(1));
+        else
+            location = goTo((&fs.getWorkingDirectory()),path);
+        if (location==nullptr)
+            cout << "The system cannot find the path specified";
+        else
+        {
+            vector <BaseFile*>:: iterator myIt;
+            bool found=false;
+            for(myIt=location->getChildren().begin(); myIt!=location->getChildren().end() && !found; myIt++)
+            {
+                if ((**myIt).getName().compare(sub) == 0)
+                    found = true;
+            }
+            if (found)
+                cout << "The file already exists";
+            else
+            {
+                File * newFile=new File(sub,size);
+                location->addFile(newFile);
+            }
+        }
+
+    }
+
+    string MkfileCommand:: toString() {return "mkfile";}
+
+    CpCommand :: CpCommand(string args) : BaseCommand(args){}
+
+        void CpCommand:: execute(FileSystem &fs)
+        {
+            string args=getArgs();
+            size_t space=args.find(' ');
+            string sub1=args.substr(0, found);
+            size_t found=args.find_last_of('/');
+            string name;
+            string path1;
+            Directory * location;
+            if(found==-1)
+            {
+                name = sub1;
+                location=(fs.getWorkingDirectory()*);
+            }
+                else
+            {
+                name=sub1.substr(found+1);
+                path1=sub1.substr(0,found);
+                if(path1.at(0)=='/')
+                    location=goTo(&fs.getRootDirectory(), path1);
+                else
+                location=goTo(&fs.getWorkingDirectory(), path1);
+            }
+            if(location==nullptr)
+                cout << "The system cannot find the path specified";
+            else
+            {
+                vector <BaseFile*>:: iterator myIt;
+                bool found=false;
+                for(myIt=location->getChildren().begin(); myIt!=location->getChildren().end() && !found; myIt++) {
+                    if ((**myIt).getName().compare(name) == 0)
+                    {
+                    found = true;
+                        if((**myIt).isFile)
+                        {
+                            int size=(**myIt).getSize();
+                            string fileName=(**myIt).getName();
+                        }
+                        else
+                        {
+                            
+                        }
+
+                    }
+
+
+                }
+        }
+
+        string CpCommand:: toString() {return "cp";}
+
 
